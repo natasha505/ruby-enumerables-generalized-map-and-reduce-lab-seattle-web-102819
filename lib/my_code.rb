203fccd2442de4005c940575ccddfb0209d0e14d
Natali kruk <natasha_505@yahoo.com>
2019-10-23 04:32:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,2 +1,26 @@
-# Your Code Here
-map_to_negativize(source_array)
+ 
+def map(source_array)
+  new_array = []
+  i = 0 
+  while i < source_array.length 
+    new_array.push(yield(source_array[i]))
+    i += 1 
+  end
+new_array
+end
+
+
+def reduce(array, starting_point=nil)
+  if starting_point
+    returned_value = starting_point
+    i = 0
+  else
+    returned_value = array[0]
+    i = 1
+  end
+  while i < array.length
+    returned_value = yield(returned_value, array[i])
+    i += 1
+  end
+  returned_value
+end
